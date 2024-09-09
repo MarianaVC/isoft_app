c3388d607ecc5bca40b22ecd6d7d7537d9c46b77
Mariana <mariana.valdivia.carbonell@gmail.com>
2024-09-08 20:12:33 -0600

    Add home screen with all vendedores info

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-
+import 'package:veta/models/vendedor.dart';
+import 'package:http/http.dart' as http;
+import 'dart:developer';
 void main() {
   runApp(const MyApp());
 }
@@ -10,84 +13,128 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Veta Company',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Veta Company',  
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: '/',
+        routes: {
+          '/home': (context) => const MyHomePage(),
+        },
+        home: const MyHomePage());
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  Future<List<Vendedor>> vendedoresFuture = getVendedor();
+  int _currentIndex = 0;
+  // final List<Widget> _screens = [
+  //   const HomeScreen(),
+  //   const SavedScreen(),
+  //   const FavoritesScreen()
+  // ];
 
-  void _incrementCounter() {
-    setState(() {
-    });
-  }
+  static Future<List<Vendedor>> getVendedor() async {
+    var url = Uri.parse("http://10.0.2.2:8000/api/vendedor/"); //remove this api url to something nicer
+    final response = await http.get(url, headers: {"Content-Type":"application/json "});
+    final List body1 = json.decode(response.body);
+    final List body = jsonDecode(utf8.decode(response.bodyBytes));
+    print(body);
+    // final body_aca = body1.runtimeType;
+    // print("body1 type $body_aca");
+
+    // print("Este $body");
+    try {
+      print("Aca");
+      print(body.map((e) => Vendedor.fromJson(e)).toList());
+    } catch (e) {
+      print(" Error! $e");
+    }
+    return body.map((e) => Vendedor.fromJson(e)).toList();
+
+}
+
+  // final List<Widget> _screens = [
+  //   const MyHomePage(),
+  // ];
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Veta Company'),
+      ),
+        bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.logout_rounded), label: 'Salir'),
+        ],
+        selectedItemColor: Theme.of(context).colorScheme.onPrimary,
+        unselectedItemColor: Theme.of(context).colorScheme.onSurfaceVariant,
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        //FutureBuilder
+        child: FutureBuilder<List<Vendedor>>(
+          future: vendedoresFuture,
+          builder: (context, snapshot) {
+            print(snapshot);
+            if (snapshot.connectionState == ConnectionState.waiting){
+              // print(snapshot.hasData);
+              //show loader
+              return const CircularProgressIndicator();
+            } else if(snapshot.hasData){
+              //display data on screen
+             final vendedores = snapshot.data!;
+             return buildVendedores(vendedores);
+            } else{
+              // if no data, show text:
+              return const Text("No hay datos disponibles");
+              
+            }
+          }
+        )
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+// function to display fetched data on screen
+Widget buildVendedores(List<Vendedor> vendedores){
+  //ListBuilder to show data in a list
+  return ListView.builder(itemCount: vendedores.length, itemBuilder: (context, index) {
+    final vendedor = vendedores[index];
+    return Container(
+      color: Colors.grey.shade300,
+      margin: const EdgeInsets.symmetric(vertical:5, horizontal: 5),
+      padding: const EdgeInsets.symmetric(vertical:5, horizontal: 5),
+      height:100,
+      width: double.maxFinite,
+      child: Row(
+        children: [
+        Expanded(flex:1, child: Text(vendedor.nombre!)),
+        Expanded(flex:1, child:Text(vendedor.paterno!)),
+        Expanded(flex:1, child:Text(vendedor.materno!)),
+        Expanded(flex:2, child:Text(vendedor.razonSocial!)),
+      ],
+    ),
+  );
+  },
+  );
 }
